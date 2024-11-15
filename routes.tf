83375d354f66a44395d52b9e9e83bd90128b7990
hastych <74678673+hastych@users.noreply.github.com>
2024-11-14 22:59:02 -0300

    routes.tf

---
+++ routes.tf
@@ -0,0 +1,48 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "${var.environment}-public-rt"
+    Environment = var.environment
+    Managed_by  = "terraform"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.availability_zones)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name        = "${var.environment}-private-rt-${count.index + 1}"
+    Environment = var.environment
+    Managed_by  = "terraform"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_app" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.private_app[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+resource "aws_route_table_association" "private_db" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.private_db[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
