896e07c19d4b44e828b722c0fb01560c926a0677
hastych <74678673+hastych@users.noreply.github.com>
2024-11-14 23:00:38 -0300

    vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+    Managed_by  = "terraform"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
+    Managed_by  = "terraform"
+  }
+}
